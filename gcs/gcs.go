5633c0880c69eabe323bf209e7878b5e4aa29595
wakiyamap <wakiyamap@gmail.com>
2018-04-03 22:30:17 +0900

    fix btc->mona

--- gcs/gcs.go
+++ gcs/gcs.go
@@ -13,7 +13,7 @@ import (
 
 	"github.com/aead/siphash"
 	"github.com/kkdai/bstream"
-	"github.com/roasbeef/btcd/wire"
+	"github.com/wakiyamap/monad/wire"
 )
 
 // Inspired by https://github.com/rasky/gcs
